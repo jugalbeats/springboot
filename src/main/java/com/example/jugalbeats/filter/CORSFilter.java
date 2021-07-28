b5638febcc0a08ce3c56348aacd1855fe5d87ed0
dhruv arora <dhruv@joshiinc.com>
2021-07-28 15:57:54 +0530

    cors change

--- src/main/java/com/example/jugalbeats/filter/CORSFilter.java
+++ src/main/java/com/example/jugalbeats/filter/CORSFilter.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
+@Order(0)
 public class CORSFilter implements Filter {
 
     @Override
