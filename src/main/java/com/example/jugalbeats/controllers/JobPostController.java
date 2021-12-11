2c6c4e91bc1d711eadf6c3b71a6466a405025a24
Rupak.joko7 <rupakkalita3@gmail.com>
2021-12-11 23:14:04 +0530

    formatted a file

--- src/main/java/com/example/jugalbeats/controllers/JobPostController.java
+++ src/main/java/com/example/jugalbeats/controllers/JobPostController.java
@@ -26,67 +26,72 @@ import com.example.jugalbeats.pojo.MusicianResponse;
 import com.example.jugalbeats.services.JobPostService;
 import com.example.jugalbeats.utils.Constants;
 import com.example.jugalbeats.utils.Utils;
+
 /*
  * dhruv:2021
  * */
 @RestController
 @RequestMapping("/api/v1/job-post")
 public class JobPostController {
-	
-	@Autowired
-	private JobPostService jobPostService;
-	
-	 @PostMapping()
-	 @Authorize
-	    public ApiResponse createJob(@RequestBody JobPostRequest job,HttpServletRequest httpRequest ) throws UnauthorizedException {
-			Utils.matchString(httpRequest.getAttribute("username").toString(), job.getUsername());
-		         ApiResponse response=jobPostService.postJob(job);
-		         if(Objects.nonNull(response)) {
-		        	 return response;
-		         }
-	            return new ApiResponse(Constants.FAILURE_CODE, Constants.FAILURE_MESSAGE);
-	        
-	    }
-	    @GetMapping("/{username}")
-	    public  ApiResponse getJobs( @PathVariable("username") String username) {
-	    	 ApiResponse response=jobPostService.getAllJob(username);
-	         if(Objects.nonNull(response)) {
-	        	 return response;
-	         }
-            return new ApiResponse(Constants.FAILURE_CODE, Constants.FAILURE_MESSAGE);
-        
+
+    @Autowired
+    private JobPostService jobPostService;
+
+    @PostMapping()
+    @Authorize
+    public ApiResponse createJob(@RequestBody JobPostRequest job, HttpServletRequest httpRequest) throws UnauthorizedException {
+        Utils.matchString(httpRequest.getAttribute("username").toString(), job.getUsername());
+        ApiResponse response = jobPostService.postJob(job);
+        if (Objects.nonNull(response)) {
+            return response;
+        }
+        return new ApiResponse(Constants.FAILURE_CODE, Constants.FAILURE_MESSAGE);
+
     }
-	    @DeleteMapping("/{username}/{jobid}")
-	    @Authorize
-	    public  ApiResponse deleteJobs( @PathVariable("username") String username,@PathVariable("jobid") String jobid,HttpServletRequest httpRequest ) throws UnauthorizedException {
-			Utils.matchString(httpRequest.getAttribute("username").toString(), username);
-	    	 ApiResponse response=jobPostService.deleteJobById(username,Long.parseLong(jobid));
-	         if(Objects.nonNull(response)) {
-	        	 return response;
-	         }
-            return new ApiResponse(Constants.FAILURE_CODE, Constants.FAILURE_MESSAGE);
-        
+
+    @GetMapping("/{username}")
+    public ApiResponse getJobs(@PathVariable("username") String username) {
+        ApiResponse response = jobPostService.getAllJob(username);
+        if (Objects.nonNull(response)) {
+            return response;
+        }
+        return new ApiResponse(Constants.FAILURE_CODE, Constants.FAILURE_MESSAGE);
+
     }
-	    @PostMapping("/{username}/apply")
-	    @Authorize
-	    public  ApiResponse applyToJob(@PathVariable("username") String username,@RequestParam Long jobId,HttpServletRequest httpRequest ) throws UnauthorizedException {
-			Utils.matchString(httpRequest.getAttribute("username").toString(), username);
-	         ApiResponse response=jobPostService.applyToJob(jobId, username);
-	         if(Objects.nonNull(response)) {
-	        	 return response;
-	         }
-         return new ApiResponse(Constants.FAILURE_CODE, Constants.FAILURE_MESSAGE);
-     
- }
-	    @GetMapping("/{username}/apply")
-	    @Authorize
-	    public  ApiResponse getApplicants(@PathVariable("username") String username,@RequestParam Long jobId,@RequestParam(required =  false) String status,HttpServletRequest httpRequest ) throws UnauthorizedException {
-			Utils.matchString(httpRequest.getAttribute("username").toString(), username);
-	         ApiResponse response=jobPostService.getJobApplicant(jobId, username,status);
-	         if(Objects.nonNull(response)) {
-	        	 return response;
-	         }
-         return new ApiResponse(Constants.FAILURE_CODE, Constants.FAILURE_MESSAGE);
-     
- }
+
+    @DeleteMapping("/{username}/{jobid}")
+    @Authorize
+    public ApiResponse deleteJobs(@PathVariable("username") String username, @PathVariable("jobid") String jobid, HttpServletRequest httpRequest) throws UnauthorizedException {
+        Utils.matchString(httpRequest.getAttribute("username").toString(), username);
+        ApiResponse response = jobPostService.deleteJobById(username, Long.parseLong(jobid));
+        if (Objects.nonNull(response)) {
+            return response;
+        }
+        return new ApiResponse(Constants.FAILURE_CODE, Constants.FAILURE_MESSAGE);
+
+    }
+
+    @PostMapping("/{username}/apply")
+    @Authorize
+    public ApiResponse applyToJob(@PathVariable("username") String username, @RequestParam Long jobId, HttpServletRequest httpRequest) throws UnauthorizedException {
+        Utils.matchString(httpRequest.getAttribute("username").toString(), username);
+        ApiResponse response = jobPostService.applyToJob(jobId, username);
+        if (Objects.nonNull(response)) {
+            return response;
+        }
+        return new ApiResponse(Constants.FAILURE_CODE, Constants.FAILURE_MESSAGE);
+
+    }
+
+    @GetMapping("/{username}/apply")
+    @Authorize
+    public ApiResponse getApplicants(@PathVariable("username") String username, @RequestParam Long jobId, @RequestParam(required = false) String status, HttpServletRequest httpRequest) throws UnauthorizedException {
+        Utils.matchString(httpRequest.getAttribute("username").toString(), username);
+        ApiResponse response = jobPostService.getJobApplicant(jobId, username, status);
+        if (Objects.nonNull(response)) {
+            return response;
+        }
+        return new ApiResponse(Constants.FAILURE_CODE, Constants.FAILURE_MESSAGE);
+
+    }
 }
