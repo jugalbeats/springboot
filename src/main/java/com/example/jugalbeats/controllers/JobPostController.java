9d84540dd04cac88123b4c0ea9b5a7c5bb950ab3
Rupak.joko7 <rupakkalita3@gmail.com>
2021-12-12 12:13:46 +0530

    removed authorize

--- src/main/java/com/example/jugalbeats/controllers/JobPostController.java
+++ src/main/java/com/example/jugalbeats/controllers/JobPostController.java
@@ -38,7 +38,7 @@ public class JobPostController {
     private JobPostService jobPostService;
 
     @PostMapping()
-    @Authorize
+    //@Authorize
     public ApiResponse createJob(@RequestBody JobPostRequest job, HttpServletRequest httpRequest) throws UnauthorizedException {
         Utils.matchString(httpRequest.getAttribute("username").toString(), job.getUsername());
         ApiResponse response = jobPostService.postJob(job);
@@ -60,7 +60,7 @@ public class JobPostController {
     }
 
     @DeleteMapping("/{username}/{jobid}")
-    @Authorize
+    //@Authorize
     public ApiResponse deleteJobs(@PathVariable("username") String username, @PathVariable("jobid") String jobid, HttpServletRequest httpRequest) throws UnauthorizedException {
         Utils.matchString(httpRequest.getAttribute("username").toString(), username);
         ApiResponse response = jobPostService.deleteJobById(username, Long.parseLong(jobid));
