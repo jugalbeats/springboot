f0852998b8b57b1fab8b350b7498fa65170504fd
Rupak.joko7 <rupakkalita3@gmail.com>
2022-02-27 13:21:38 +0530

    added fix for foreign key

--- src/main/java/com/example/jugalbeats/dao/CalendarRepository.java
+++ src/main/java/com/example/jugalbeats/dao/CalendarRepository.java
@@ -2,6 +2,7 @@ package com.example.jugalbeats.dao;
 
 import com.example.jugalbeats.models.Calendar;
 import com.example.jugalbeats.models.UsersModel;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.transaction.Transactional;
@@ -13,5 +14,8 @@ public interface CalendarRepository extends CrudRepository<Calendar, Long>{
     List<Calendar> findByUserNameArtist(UsersModel usersModel);
 
     Calendar findByUserNameArtistAndDates(UsersModel usersModel, String dates);
+
+    @Query(value = "DELETE FROM calendar where user_name_artist = ?1 and date = ?2", nativeQuery = true)
+    void deleteByIdForDate(UsersModel usersModel, String date);
 
 }
